1f2b8a1cf8aa81a3d22f7d32dc1b5579d1de9642
carmela121 <mcarmencarmona@hotmail.com>
2016-01-29 14:59:28 +0000

    Text input and submit button

---
+++ js/app.js
@@ -0,0 +1,53 @@
+
+
+// var $countriesList = window.countries;
+
+// document.getElementById('#new-country-form').onkeydown = function(e){
+//    if(e.keyCode == 13){
+  
+//    }
+// };
+
+// document.onkeydown=function(){
+//     if(window.event.keyCode=='13'){
+//         submitForm();
+//     }
+// }
+
+// function submitForm(){
+//     document.new-country-form.submit();
+//     document.new-country-form.method='post';
+// }
+// document.onkeydown=function(){
+//     if(window.event.keyCode=='13'){
+//         return submitForm();
+//     }
+// }
+
+$(document).ready(function() {
+
+  function matchCountry(text) {
+    if (countries.indexOf(text) !== -1){
+    matchCountry('#correctAnswers');
+    }
+  }
+  
+  // $('#new-country-form').on("click", function() {
+    // var key = e.which;
+    // if (key == 13) {
+// As ASCII code for ENTER key is "13"
+  $('#new-country-form').on("submit", function(event) {
+    event.preventDefault();
+    var text = $("#new-country-input").val();
+    console.log(text);
+    $("#new-country-input").val("");
+    matchCountry(text);
+  }); // Submit form code
+  // });
+});
+
+// var countryMatch = window.countries;
+// countries.indexOf(countryMatch);
+
+
+
