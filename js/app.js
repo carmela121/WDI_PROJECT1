01bded2919a7de5f7334cc33cea53a160d16ed86
carmela121 <mcarmencarmona@hotmail.com>
2016-01-29 15:32:03 +0000

    Timer working

--- js/app.js
+++ js/app.js
@@ -25,25 +25,66 @@
 // }
 
 $(document).ready(function() {
-
+//searches for a match on the country list
   function matchCountry(text) {
     if (countries.indexOf(text) !== -1){
     console.log("it's a match!");
     }
   }
   
-  // $('#new-country-form').on("click", function() {
-    // var key = e.which;
-    // if (key == 13) {
-// As ASCII code for ENTER key is "13"
+
   $('#new-country-form').on("submit", function(event) {
     event.preventDefault();
     var text = $("#new-country-input").val();
     console.log(text);
     $("#new-country-input").val("");
     matchCountry(text);
-  }); // Submit form code
-  // });
+  }); // Submit form 
+  //
+function printCorrectAnswer(text){
+  if (matchCountry === countries){
+
+  }
+  //timer
+}
+
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#screen');
+    startTimer(fiveMinutes, display);
+};
+
+
 });
 
 // var countryMatch = window.countries;
