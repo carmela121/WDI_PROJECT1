5a4e2a0f543e29ccf376e0d417de717f6a1aca11
carmela121 <mcarmencarmona@hotmail.com>
2016-02-01 11:37:08 +0000

    commiting before code shuffle

--- js/app.js
+++ js/app.js
@@ -29,20 +29,39 @@ $(document).ready(function() {
   var time = 60 * 3;
   var timer;
   var $answers = $('#printAnswers');
-  var $score = $('player1')
+  var player1 = 0;
+  var player2 = 0;
+  var player = 1;
 
   //searches for a match on the country list
   function matchCountry(text) {
     console.log(countries.indexOf(text.toLowerCase()))
     if (countries.indexOf(text.toLowerCase()) !== -1){
-      console.log("it's a match!");
-      $('#printAnswers').append('<li>' + text + '</li>');
+      if(player === 1) {
+        player1++;
+      } else {
+        player2++;
+      }
+      $('.score1').text("player1 "+ player1);
+      $('.score2').text("player2 "+ player2);
+      // display scores
+      // $('#printAnswers').append('<li>' + text + '</li>');
     } else {
       console.log("Sorry, please try again");
     }
   }
 
-  //Change turn
+// if (player1 < player2){
+//   return ("Player 1 wins!");
+// } else if (player1 > player2){
+//   return ("Player 2 wins!");
+// } else {
+//   return ("It's a tie");
+// }
+
+
+
+  
 
   
  $('#new-country-form').on("submit", function(event) {
@@ -111,11 +130,25 @@ $(document).ready(function() {
       seconds = seconds < 10 ? "0" + seconds : seconds;
 
       display.text(minutes + ":" + seconds);
-      var player1;
-      var player2;
+      
       if(time === 0) {
         clearInterval(timer)
         // change player & reset the board;
+        if(player === 1) {
+          player = 2;
+          // reset the board
+          $("#new-country-input").addClass('hidden');
+          $('button').show();
+        } else {
+          // end the game
+          // find winner
+
+          if(player1 > player2) {
+            $(".winner").text("Player 1 wins!")
+          } else {
+            $(".winner").text("Player 2 wins!")
+          }
+        }
       }
     }, 1000)
   }
